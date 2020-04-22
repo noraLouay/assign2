70043a66b912d21afee6b93906be31269c41b479
Nora AL Asbahi <nora.asbahi@gmail.com>
2020-04-22 14:59:26 +0300

    quadrangle class add.

---
+++ src/Quadrangle2D.java
@@ -0,0 +1,17 @@
+public class Quadrangle2D{
+    public Point2D point1,point2,point3,point4;
+    private int area;
+
+    public Quadrangle2D(Point2D p1, Point2D p2,Point2D p3,Point2D p4) {
+        if (p1.isIdentical(p2) || p1.isIdentical(p3) || p1.isIdentical(p4) || p2.isIdentical(p3) || p2.isIdentical(p3) || p3.isIdentical(p4)) {
+            point1 = p1;
+            point2 = p2;
+            point3 = p3;
+            point4 = p4;
+        } else {
+            System.out.println("Invalid points please,reorder them or change them! ");
+        }
+    }
+
+
+}
